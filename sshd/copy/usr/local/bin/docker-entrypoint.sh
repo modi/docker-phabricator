a61ccb918f43540bf0214960c3102d28474dc4d0
Mo Di <e40dff@outlook.com>
2020-10-15 11:09:35 +0800

    把 SSH 服务端证书放到挂载数据卷的路径，以解决服务端证书重新生成引起客户端报错的问题

--- sshd/copy/usr/local/bin/docker-entrypoint.sh
+++ sshd/copy/usr/local/bin/docker-entrypoint.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-if [[ ! -e /etc/ssh/ssh_host_dsa_key ]]; then
-    ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -t dsa -N ''
-    ssh-keygen -q -f /etc/ssh/ssh_host_ecdsa_key -t ecdsa -N ''
-    ssh-keygen -q -f /etc/ssh/ssh_host_ed25519_key -t ed25519 -N ''
-    ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -t rsa -N ''
+if [[ ! -d /data/ssh ]]; then
+    mkdir /data/ssh
+    ssh-keygen -q -f /data/ssh/ssh_host_dsa_key -t dsa -N ''
+    ssh-keygen -q -f /data/ssh/ssh_host_ecdsa_key -t ecdsa -N ''
+    ssh-keygen -q -f /data/ssh/ssh_host_ed25519_key -t ed25519 -N ''
+    ssh-keygen -q -f /data/ssh/ssh_host_rsa_key -t rsa -N ''
 fi
 
 exec "$@"
