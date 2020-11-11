80435dd4c1e858c035d6e9e36e6ad3249fbe2365
Mo Di <e40dff@outlook.com>
2020-11-11 11:37:41 +0800

    修正 phd 管理进程和 Aphlict 通知服务进程的信号处理

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 - 克隆本仓库
 - 把 `.env.example` 复制为 `.env`（如果宿主机的 8801、2201、22280 三个端口被其他程序占用，可在 `.env` 里指定其他端口号）
-- 执行 `docker-compose run --rm fpm phabricator-install.sh`
+- 执行 `docker-compose run --rm fpm phabricator-configure.sh`
 - 执行 `docker-compose up -d`
 - 把域名 `phab.localhost` 解析到 `127.0.0.1`
 
--- sshd/copy/usr/local/bin/docker-entrypoint.sh
+++ sshd/copy/usr/local/bin/docker-entrypoint.sh
@@ -4,8 +4,17 @@ set -e
 
 if [[ ! -e /data/sshd/ssh_host_dsa_key ]]; then
     ssh-keygen -q -f /data/sshd/ssh_host_dsa_key -t dsa -N ''
+fi
+
+if [[ ! -e /data/sshd/ssh_host_ecdsa_key ]]; then
     ssh-keygen -q -f /data/sshd/ssh_host_ecdsa_key -t ecdsa -N ''
+fi
+
+if [[ ! -e /data/sshd/ssh_host_ed25519_key ]]; then
     ssh-keygen -q -f /data/sshd/ssh_host_ed25519_key -t ed25519 -N ''
+fi
+
+if [[ ! -e /data/sshd/ssh_host_rsa_key ]]; then
     ssh-keygen -q -f /data/sshd/ssh_host_rsa_key -t rsa -N ''
 fi
 
